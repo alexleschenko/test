02f8fadcd051d16ee965b59ba336efe709c718d0
Alex Leschenko <a_leshenko@tut.by>
2016-09-07 12:21:57 +0300

    create result page

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.http import request
 from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404
@@ -7,21 +8,21 @@ from django.views.generic.list import ListView
 
 import models
 
-class MainView(TemplateView):
+class MainView(TemplateView): #отображение главной страницы
     template_name = 'main.html'
 
 
-class TestView(ListView):
+class TestView(ListView): #отображение списка тестов
 
     model = models.Group
     template_name = 'test.html'
     context_object_name = 'packs'
 
-class QuestionView(ListView):
+class QuestionView(ListView): # отображение вопросов из теста
     template_name = 'test2.html'
     context_object_name = 'questions'
 
-    def get_queryset(self):
+    def get_queryset(self): # перехват номера теста через параметр в url 'test/(?P<group_id>\d+)/$'
         url_parametr = self.kwargs['group_id']
         q = models.Question.objects.filter(group_id=url_parametr)
 
@@ -30,7 +31,7 @@ class QuestionView(ListView):
 class TestResult(View):
     template_name = 'result.html'
 
-    def post(self, request):
+    def post(self, request): #обработка результатов: вывод списка правильных ответов и общего количества вопросов
         group_id = self.request.POST.get('group')
         answers = models.Question.objects.filter(group_id=group_id).values('id', 'correct')
         correct = 0
